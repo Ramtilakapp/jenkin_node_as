6efb7b6f1e075ecc2cb4097fcc7a5f9232c3b8f1
ashi-2212 <ashi.singh@appinventiv.com>
2024-08-28 11:49:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World, this is Ashi Singh....\n');
+  res.end('Hello World! this is Ashi Singh.\n');
 });
 
 // Listen on port 3000 and IP 127.0.0.1.
